cef258fe1f5b8c885965a1b2ed16a26d49db6ea8
Zac Bentley <zbbentley@gmail.com>
2017-03-15 16:05:59 -0400

    Clean up usage

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ import (
 
 func parseArgs(args []string) injector.CodeToInject {
 	var (
-		app     = kingpin.New("completion", "My application with bash completion.").Version("0.0.1")
-		pid     = app.Flag("pid", "ZBTODO timeout").Short('p').Required().Int()
-		debug   = app.Flag("debug", "ZBTODO timeout").Short('d').Bool()
-		timeout = app.Flag("timeout", "ZBTODO timeout").Short('t').Default("5s").Duration()
-		code    = app.Flag("code", "ZBTODO timeout").Short('c').Default("require Carp unless exists($INC{'Carp.pm'}); local $Carp::MaxArgLen = 0; local $Carp::MaxArgNums = 0; print $fh Carp::longmess('INJECT')").String()
-		force   = app.Flag("force", "ZBTODO force").Short('f').Bool()
-		signals = app.Flag("signals", "ZBTODO signz").Short('s').Default("false").Bool()
+		app     = kingpin.New("gdb-inject-perl", "Inject code into a running Perl process, using GDB. Dangerous, but useful for getting debug info in a pinch.\nSee https://github.com/zbentley/gdb-inject-perl for more info.").Version("0.1.0")
+		pid     = app.Flag("pid", "Process ID of the Perl process to inject code into. PID can be any kind of Perl process: embedded, mod_perl, simple script, etc.\nThis option is required.").Short('p').Required().Int()
+		debug   = app.Flag("debug", "Enable (lots of) debug output").Short('d').Bool()
+		timeout = app.Flag("timeout", "Time to wait until PID runs CODE. Accepts a duration string, e.g. '10s' or '2m'.\nIf CODE does not run within TIMEOUT, this program will exit with an error unless '--signals' is supplied.\nDefaults to '5s'.").Short('t').Default("5s").Duration()
+		code    = app.Flag("code", "String of code that will be injected into the Perl process at PID and run.\nDefaults to a string that prints out the current Perl call stack of the process at PID.").Short('c').Default("require Carp unless exists($INC{'Carp.pm'}); local $Carp::MaxArgLen = 0; local $Carp::MaxArgNums = 0; print $fh Carp::longmess('INJECT')").String()
+		force   = app.Flag("force", "Disable validation of CODE via the local Perl interpreter. Not recommended.\nDefaults to false.").Short('f').Bool()
+		signals = app.Flag("signals", "If set, enables the sending of signals to stuck Perl processes that will not run CODE.\nThis can cause code to be run even if Perl is stuck in a blocking system call, and should work even in some cases when signals have been blocked by Perl.\nThe user will be prompted on the terminal for a signal to send.\nDefaults to disabled.").Short('s').Default("false").Bool()
 		signalmap map[uint32]string
 	)
 	kingpin.MustParse(app.Parse(args))
